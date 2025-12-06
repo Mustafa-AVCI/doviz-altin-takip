a344f3b8808776e746e89035621294b884e046eb
Mustafa <115926943+Mustafa-AVCI@users.noreply.github.com>
2025-12-06 21:36:03 +0300

    Delete İleri_teknik_proje directory

--- İleri_teknik_proje/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("add-btn").addEventListener("click", () => {
-    const list = document.getElementById("feature-list");
-
-    const item = document.createElement("li");
-    item.textContent = "Yeni özellik eklendi (" + new Date().toLocaleTimeString() + ")";
-
-    list.appendChild(item);
-});
